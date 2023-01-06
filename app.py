dab6edd397562ec02a39451f643361454f8bc4de
Esrah Zahid <62853038+ziiayi@users.noreply.github.com>
2023-01-06 21:44:23 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from pywebio import start_server
 from pywebio.input import *
 from pywebio.output import *
 from pywebio.session import defer_call, info as session_info, run_async, run_js
-#from pywebio.platform.tornado_http import start_server as start_http_server
+from pywebio.platform.tornado_http import start_server as start_http_server
 from pywebio import start_server as start_ws_server
 from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives import hashes
@@ -91,10 +91,10 @@ if __name__ == "__main__":
     parser.add_argument("-p", "--port", type=int, default=8080)
     parser.add_argument("--http", action="store_true", default=False, help='Whether to enable http protocol for communicates')
     args = parser.parse_args()
-    #if args.http:
-    #    start_http_server(main, port=args.port)
-    #else:
+    if args.http:
+        start_http_server(main, port=args.port)
+    else:
         # Since some cloud server may close idle connections (such as heroku),
         # use `websocket_ping_interval` to  keep the connection alive
-    start_ws_server(main, port=args.port, websocket_ping_interval=30)
+        start_ws_server(main, port=args.port, websocket_ping_interval=30)
     #start_server(main, debug=True, port=8080, cdn=False)
